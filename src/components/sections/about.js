7c11448dc07d774d21dad25f3445a0b8b8a716c6
hari-599 <harisdarsan@gmail.com>
2021-12-09 14:23:35 +0530

    sdahgoashdoghasdoighoiasdg

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -157,7 +157,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src=".\IMG_20200313_203243_659.jpg"
+              src="../../images/darsan.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
