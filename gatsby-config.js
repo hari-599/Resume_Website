8afee3809318b5f9d2ae02775016c2f9bcf3b88d
dart-599 <harisdarsan@gmail.com>
2026-03-23 22:28:29 +0530

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,8 @@ module.exports = {
     title: 'Hari Sudarsan',
     description:
       'Hari Sudarsan is an applied machine learning engineer who specializes in building deployable machine learning systems.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://harisdarsan.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Hari Sudarsan',
+        short_name: 'Hari',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
